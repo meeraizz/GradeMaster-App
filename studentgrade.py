7bcf69ccd3430fe963c99aad1f88f3c43d407b80
adilahisham <nuradilahnorhisham@gmail.com>
2024-05-08 16:14:00 +0800

    Text box added

--- studentgrade.py
+++ studentgrade.py
@@ -35,8 +35,12 @@ class resultClass:
         self.txt_student = ttk.Combobox(self.root, textvariable=self.var_roll, values=self.roll_list, font=("times new roman", 15, "bold"), state='readonly', justify=CENTER)
         self.txt_student.place(x=280,y=100,width=200)
         self.txt_student.set("Select")
+        btn_search=Button(self.root,text='Search',font=("times new roman",15,"bold"),bg="#03a9f4",fg="white",cursor="hand2").place(x=500,y=100,width=120,height=28)
 
-
+        txt_name=Entry(self.root,textvariable=self.var_name,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=160,width=300)
+        txt_course=Entry(self.root,textvariable=self.var_course,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=220,width=300)
+        txt_marks=Entry(self.root,textvariable=self.var_marks,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=280,width=300)
+        txt_full_marks=Entry(self.root,textvariable=self.var_full_marks,font=("times new roman",20,"bold"),bg="lightyellow").place(x=280,y=340,width=300)
 
 
 
