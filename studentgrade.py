bdba055417b8027e0918bf02134a8f2ae06bd6b9
adilahisham <nuradilahnorhisham@gmail.com>
2024-05-15 14:35:26 +0800

    Added createdatabase file from Adilah to Adilah(tutorial)

--- studentgrade.py
+++ studentgrade.py
@@ -21,6 +21,7 @@ class resultClass:
         self.var_marks=StringVar()
         self.var_full_marks=StringVar()
         self.roll_list=[]
+        self.fetch_roll()
 
 
         #===widgets===
@@ -35,7 +36,7 @@ class resultClass:
         self.txt_student = ttk.Combobox(self.root, textvariable=self.var_roll, values=self.roll_list, font=("times new roman", 15, "bold"), state='readonly', justify=CENTER)
         self.txt_student.place(x=280,y=100,width=200)
         self.txt_student.set("Select")
-        btn_search=Button(self.root,text='Search',font=("times new roman",15,"bold"),bg="#03a9f4",fg="white",cursor="hand2").place(x=500,y=100,width=100,height=28)
+        btn_search=Button(self.root,text='Search',font=("times new roman",15,"bold"),bg="#03a9f4",fg="white",cursor="hand2",command=self.search()).place(x=500,y=100,width=100,height=28)
 
         txt_name=Entry(self.root,textvariable=self.var_name,font=("times new roman",20,"bold"),bg="lightyellow",state='readonly').place(x=280,y=160,width=320)
         txt_course=Entry(self.root,textvariable=self.var_course,font=("times new roman",20,"bold"),bg="lightyellow",state='readonly').place(x=280,y=220,width=320)
@@ -47,9 +48,33 @@ class resultClass:
         btn_add=Button(self.root,text="Submit",font=("times new roman",15),bg="lightgreen",activebackground="lightgreen",cursor="hand2").place(x=300,y=420,width=120,height=35)
         btn_add=Button(self.root,text="Clear",font=("times new roman",15),bg="lightgrey",activebackground="lightgrey",cursor="hand2").place(x=430,y=420,width=120,height=35)
 
+#===================================================
+    def fetch_roll(self):
+        conn=sqlite3.connect(database="GradeMaster.db")
+        cur=conn.cursor()
+        try:
+            cur.execute("SELECT roll FROM student")
+            rows=cur.fetchall()
+            if len(rows)>0:
+                for row in rows:
+                    self.roll_list.append(row[0])
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to {str(ex)}")
 
-
-
+    def search(self):
+        conn=sqlite3.connect(database="GradeMaster.db")
+        cur=conn.cursor()
+        try:
+            cur.execute("SELECT name,course* FROM student WHERE roll =?",(self.var_roll.get(),))
+            rows=cur.fetchone()
+            if row!=None:
+                self.var_name.set("row[0]")
+                self.var_course.set("row[1]")
+            else :
+                messagebox.showerror("Error","No record found",parent=self.root)
+        
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to {str(ex)}")
 
 
 
